26e7bf93c567869dd729f1dbedffb90da57c6718
Chance Hudson <jchancehud@gmail.com>
2019-03-05 21:06:54 -0600

    Add path handling to redirect

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = (req, res) => {
-  res.writeHead(301, { 'Location': 'https://limeapartments.com' });
+  res.writeHead(301, { 'Location': `https://limeapartments.com${req.url}` });
   res.end();
 };
